eec36a193a9106246db1bb8ef5a184ec34013acb
Leandro <leandro.c.lezcano@gmail.com>
2014-11-20 18:28:32 -0300

    sendMail Service (send from a gmail account)

--- src/main/java/com/soap/ws/mail/Mail.java
+++ src/main/java/com/soap/ws/mail/Mail.java
@@ -1,9 +1,16 @@
 package com.soap.ws.mail;
 
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.jws.WebService;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @WebService(endpointInterface = "com.soap.ws.mail.IMail")
 public class Mail implements IMail {
@@ -14,11 +21,42 @@ public class Mail implements IMail {
 	
 	private Pattern pattern;
 	private Matcher matcher;
+	
+	
+	private String host = "smtp.gmail.com";
 
 	@Override
 	public boolean sendMail(SendMailInfo mailInfo) {
-		// TODO Auto-generated method stub
-		return true;
+        Properties props = System.getProperties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", mailInfo.from);
+        props.put("mail.smtp.password", mailInfo.pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(mailInfo.from));
+            
+            for( String iAddress : mailInfo.to ) {
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(iAddress));
+            }         
+            
+            message.setSubject(mailInfo.subject);
+            message.setText(mailInfo.body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, mailInfo.from, mailInfo.pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false ;
+        }		
+        return true;
 	}
 
 	@Override
